09ec72d8dbd22945208cc02da696415028484367
满满 <zhangchao@fujiacaifu.com>
2017-11-06 22:26:59 +0800

    新增fjpublish auth命令,新增fjpublish env命令选项--ssh

--- bin/fjpublish.js
+++ bin/fjpublish.js
@@ -2,12 +2,13 @@
 
 const pkg = require('../package.json');
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const chalk = require('chalk');
 const program = require('commander');
 const is = require('is');
 const inquirer = require('inquirer');
-const { spawn, execSync } = require('child_process');
+const { spawn, execSync, exec } = require('child_process');
 const Fjpublish = require('../lib/index.js');
 const pull = require('../lib/fjpublish_pull.js');
 const prompt = require('../lib/fjpublish_prompt.js');
@@ -37,7 +38,7 @@ const isObject = is.object,
 process.on('uncaughtException', (err) => {
     return error(err.stack);
 });
-process.on('unhandledRejection', function(err, p) {
+process.on('unhandledRejection', (err, p) => {
     return error(err.stack);
 });
 
@@ -51,23 +52,42 @@ program
     .description('Generate a default configuration for reference')
     .action((cmd) => {
         COMMAND = 'init';
-        let confPath = path.resolve(process.cwd(), 'fjpublish.config.js');
-        let copySourcePath = confPath.replace(/\s/g, '\\ '); //把空格转义
-        let copyObjectPath = path.resolve(__dirname, '../lib/fjpublish.config.js').replace(/\s/g, '\\ '); //把空格转义
-        let cp = `cp -f ${copyObjectPath} ${copySourcePath}`;
-        if (fs.existsSync(confPath)) {
-            inquirer.prompt([{
-                type: 'confirm',
-                name: 'init',
-                message: "There is a 'fjpublish.config.js' file in the current directory. Continue operation will overwrite. Are you sure?",
-                default: false,
-            }]).then((answers) => {
-                if (!answers.init) return warning('cancelled', false);
-                execSync(cp);
+        let confPath = path.join(process.cwd(), 'fjpublish.config.js');
+        let p = fs.existsSync(confPath) ? inquirer.prompt([{
+            type: 'confirm',
+            name: 'init',
+            message: "There is a 'fjpublish.config.js' file in the current directory. Continue operation will overwrite. Are you sure?",
+            default: false,
+        }]) : Promise.resolve({ init: true });
+        p.then(({ init }) => {
+            if (!init) return warning('cancelled', false);
+            let fileStream = fs.createReadStream(path.join(__dirname, '../lib/fjpublish.config.js'));
+            fileStream.pipe(fs.createWriteStream(confPath));
+            fileStream.on('end', () => {
+                success('Init success');
             });
-        } else {
-            execSync(cp);
-        };
+        });
+    });
+
+program
+    .command('auth <server>')
+    .usage('<server> [options]')
+    .description('authenticate the public key to remote server')
+    .option('-k, --key <path>', 'Select the pubic keys that need to be authenticated')
+    .action((server, { key }) => {
+        COMMAND = 'auth';
+        let defaultKey = `${os.homedir()}/.ssh/id_rsa.pub`;
+        if (!key) assert(fs.existsSync(defaultKey), 'error', "File '~/.ssh/id_rsa.pub' is not found, please check or select a pubic keys that need to be authenticated");
+        if (key) assert(path.isAbsolute(key) && path.extname(key) === '.pub', 'error', "please select a public key and path must be absolute path, eg '/abc/cde.pub'");
+        if (key) assert(fs.existsSync(key), 'error', "Public key you choice is not exist");
+        if (!key) key = defaultKey;
+        exec(`cat ${key} | ssh ${server} 'mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys'`, (err, stdout, stderr) => {
+            if (err) {
+                error(`auth error: ${err}`);
+                return;
+            };
+            success('auth success');
+        });
     });
 
 program
@@ -102,6 +122,7 @@ program
     .option('-d, --diff [env]', 'Only publish modified files')
     .option('--merge [env]', 'Merge the current version with the previous version')
     .option('-t, --tag <message>', 'Create a tag on publish')
+    .option('--ssh <ssh>', 'Create a tag on publish')
     .option('--cmd <command>', 'Set node build command')
     .option('--check', 'Do not run the Task and check parameters')
     .option('--parallel', 'Parallel publish')
@@ -116,12 +137,12 @@ program
         COMMAND = true;
         let { select, multiple, yes } = cmd;
         assert(env || select || multiple, 'error', "You must choose an environment to publish, please use 'fjpublish env <env> [option]' or 'fjpublish env -s|m [option]'")
-        let module, choices;
         let configPath = getConfigPath(cmd.parent);
         let config = require(configPath);
         let modules = config.modules;
         assert(!isUndefined(modules), 'error', "Config option 'modules' is required");
         if (env) {
+            let module;
             let configIsArray = isArray(modules);
             let nameArr = env.split(',').map(v => {
                 if (module = configIsArray ? modules.find(vv => v === vv.env) : modules[v]) {
@@ -130,39 +151,19 @@ program
                     error(`The selected environment '${v}' does not exist`);
                 };
             });
-            if (yes) return MAINFUNC(config, env.split(','), cmd);
+            if (yes) return envMainFunction(config, env.split(','), cmd);
             inquirer.prompt([{
                 type: 'confirm',
                 name: 'confirm',
                 message: `Confirm publish to ${chalk.red(nameArr.join(','))} ?`,
                 default: false,
             }]).then((answers) => {
-                if (answers.confirm) MAINFUNC(config, env.split(','), cmd);
+                if (answers.confirm) envMainFunction(config, env.split(','), cmd);
             });
         } else {
-            if (isArray(modules)) {
-                choices = modules.map(v => ({
-                    name: `${chalk.green(v.env)}  ${v.name}  ${v.ssh.host}`,
-                    value: v.env
-                }));
-            } else if (isObject(modules)) {
-                choices = [];
-                Object.keys(modules).forEach(v => {
-                    let obj = modules[v];
-                    choices.push({
-                        name: `${chalk.green(obj.env)}  ${obj.name}  ${obj.ssh.host}`,
-                        value: obj.env
-                    });
-                });
-            };
-            inquirer.prompt([{
-                type: multiple ? 'checkbox' : 'list',
-                name: 'env',
-                message: 'Please select the environment to publish?',
-                choices,
-            }]).then((answers) => {
+            selectEnv(modules, 'Please select the environment to publish?', multiple).then((answers) => {
                 if (isArray(answers.env) && !answers.env.length) return warning('Not to select');
-                MAINFUNC(config, isArray(answers.env) ? answers.env : [answers.env], cmd);
+                envMainFunction(config, isArray(answers.env) ? answers.env : [answers.env], cmd);
             });
         };
     })
@@ -190,13 +191,14 @@ program
     })
 
 
-program.on('--help', function() {
+program.on('--help', function () {
     console.log('');
     console.log('  e.g:');
     console.log('');
     console.log('    $ fjpublish init');
     console.log('    $ fjpublish list');
     console.log('    $ fjpublish env [env] [option]');
+    console.log('    $ fjpublish auth <server>');
     console.log('    $ fjpublish recover <env> [option]');
     console.log('');
 });
@@ -207,8 +209,16 @@ if (typeof COMMAND === 'undefined') {
     error("The command not selected or no exist, you can get help by enter 'fjpublish -h'");
 };
 
+function envMainFunction(config, env, cmd) {
+    let p = cmd.ssh ? sshParse(cmd.ssh, env) : Promise.resolve();
+    p.then(ssh => {
+        if (ssh) cmd.ssh = ssh;
+        MAINFUNC(config, env, cmd);
+    });
+};
+
 function MAINFUNC(config, env, cmd) {
-    let { use = { pull, prompt, git, builder, compress, sftp, shell, push }, commit, rebase, tag, push: gitpush, nobuild, nobackup, nohistory, merge, diff, prompt: usePrompt, check, parallel, parent, arg, cmd: command } = cmd;
+    let { use = { pull, prompt, git, builder, compress, sftp, shell, push }, ssh, commit, rebase, tag, push: gitpush, nobuild, nobackup, nohistory, merge, diff, prompt: usePrompt, check, parallel, parent, arg, cmd: command } = cmd;
     let extend = {};
     env = env.map(v => ({
         env: v,
@@ -217,8 +227,9 @@ function MAINFUNC(config, env, cmd) {
         nohistory: cmdExtendModules(v, nohistory),
         merge: cmdExtendModules(v, merge),
         diff: cmdExtendModules(v, diff),
+        ssh: cmdExtendModules(v, ssh ? ssh.env : void 0, ssh),
         tag,
-        buildCommand: command
+        buildCommand: command,
     }));
     if (arg) {
         let parse = cmdCustomArgParse(arg);
@@ -258,14 +269,14 @@ function MAINFUNC(config, env, cmd) {
         }];
         extend._promptSyncModule = ['nobuild', 'nobackup', 'merge']
     };
-
     let fjpublish = Fjpublish(config, env);
     Object.keys(use).forEach(v => fjpublish.use(use[v]));
     fjpublish.metadata(extend);
     fjpublish.start(true);
 };
 
-//asd:cbd@a-b-c
+//abc:cbd@a-b-c
+//<key>[:val][@envs]
 function cmdCustomArgParse(arg, exec) {
     return arg.split(',').map(v => {
         return (exec = /^(\w+)(?::(\w+))?(?:@(\w+(?:-\w+)*))?$/.exec(v)) ? {
@@ -274,6 +285,56 @@ function cmdCustomArgParse(arg, exec) {
             env: exec[3] ? exec[3].replace(/-/g, ',') : true,
         } : null;
     });
+};
+
+function selectEnv(modules, tips, multiple) {
+    let choices;
+    if (isArray(modules)) {
+        choices = modules.map(v => ({
+            name: `${chalk.green(v.env)}  ${v.name}  ${v.ssh.host}`,
+            value: v.env
+        }));
+    } else if (isObject(modules)) {
+        choices = [];
+        Object.keys(modules).forEach(v => {
+            let obj = modules[v];
+            choices.push({
+                name: `${chalk.green(obj.env)}  ${obj.name}  ${obj.ssh.host}`,
+                value: obj.env
+            });
+        });
+    };
+    return inquirer.prompt([{
+        type: multiple ? 'checkbox' : 'list',
+        name: 'env',
+        message: tips,
+        choices,
+    }]);
+};
+
+//test:root@123.12.23.33:22#abcdefg
+//[env:]<username><@host>[:port][#password]
+function sshParse(str, env) {
+    let exec = /^(?:([^:]+):)?([a-zA-Z0-9\-\._]+)@([^:#]+)(?::([0-9]+))?(?:#(.+))?$/.exec(str);
+    assert(exec, 'error', 'ssh is no vaild');
+    let ssh = {
+        env: env.length > 1 ? exec[1] : env[0],
+        username: exec[2],
+        host: exec[3],
+        port: exec[4],
+        password: exec[5],
+    };
+    if (env.length === 1 && exec && exec[1]) warning("When you publish only one environment, the --ssh parameter 'env' is not required", false);
+    if (env.length > 1 && !ssh.env) error("When multiple environments are selected, the --ssh parameter 'env' should clearly indicate which environment is set up");
+    if (env.length > 1 && !env.includes(ssh.env)) error('The env that --ssh set up  is not include in this publish task');
+    return isUndefined(ssh.password) ? inquirer.prompt([{
+        type: 'password',
+        name: 'password',
+        message: `env ${ssh.env}'s password`,
+    }]).then(answers => {
+        ssh.password = answers.password;
+        return Promise.resolve(ssh);
+    }) : Promise.resolve(ssh);
 };
 
 function cmdExtendModules(singleEnv, optionEnv, val = true) {
